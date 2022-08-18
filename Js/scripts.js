89dc5b5bb98633d56f0520c74a4337a49741eb7e
RandyMj <randymotaj@gmail.com>
2022-08-18 10:28:30 -0400

    Adding Desktop menu, Mobile menu and Shopping Cart html structure and css styles

--- Js/scripts.js
+++ Js/scripts.js
@@ -2,13 +2,41 @@ const navEmail = document.querySelector(".navbar-email");
 const deskMenu = document.querySelector(".desktop-menu");
 const mobMenu = document.querySelector(".mobile-menu");
 const burgerMenuIcon = document.querySelector(".menu");
+const shoppingCartIcon = document.querySelector(".navbar-shopping-cart");
+const shoppingCarProductDetails = document.querySelector(".product-detail");
 
 navEmail.addEventListener("click", showHideDesktopMenu);
 burgerMenuIcon.addEventListener("click", showHideBurgerMenu);
+shoppingCartIcon.addEventListener("click", showHideshoppingCartDetails);
 
 function showHideDesktopMenu() {
+  let IsShoppingCartClosed =
+    shoppingCarProductDetails.classList.contains("inactive");
+
+  if (!IsShoppingCartClosed) {
+    shoppingCarProductDetails.classList.add("inactive");
+  }
+
   deskMenu.classList.toggle("inactive");
 }
+
 function showHideBurgerMenu() {
+  let IsShoppingCartClosed =
+    shoppingCarProductDetails.classList.contains("inactive");
+
+  if (!IsShoppingCartClosed) {
+    shoppingCarProductDetails.classList.add("inactive");
+  }
+
   mobMenu.classList.toggle("inactive");
+}
+
+function showHideshoppingCartDetails() {
+  let IsMobileMenuClosed = mobMenu.classList.contains("inactive");
+
+  if (!IsMobileMenuClosed) {
+    mobMenu.classList.add("inactive");
+  }
+
+  shoppingCarProductDetails.classList.toggle("inactive");
 }
